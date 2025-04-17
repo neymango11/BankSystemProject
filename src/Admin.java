f960d633eb99c01a496b32067892b143bd97b77d
nebilll <tossaucy@gmail.com>
2025-04-16 21:33:07 -0400

    Nebil update 2
    Comments on Admin class
    Updated User class (my main class)

--- src/Admin.java
+++ src/Admin.java
@@ -1,2 +1,26 @@
-users.add(newUser);
+public class Admin  extends User{
 
+
+
+    // methods to create a new user
+
+
+
+
+    // methods to remove a user by ID
+
+
+
+
+    // methods close (deactivate) a users account
+
+
+
+
+    // methods viewing all users transactions
+
+
+
+
+
+}
--- src/User.java
+++ src/User.java
@@ -1,46 +1,53 @@
+
 public class User {
     private String username;
     private String password;
     private String role;
     private int userID;
 
+    // Constructors & parameters
    public User(String username, String password, String role, Integer userID) {
-        this.username = username;
-        this.password = password;
-        this.role = role;
-        this.userID = userID;
+        this.username = username; // users login name
+        this.password = password; // users password
+        this.role = "STANDARD USER"; // the users role (STANDARD USER or ADMIN)
+        this.userID = userID; // a unique identifier for this user
     }
 
-    //Getters
+    // Getter
     public String getUsername() {
-       return username;
+       return username; // retrieving username
     }
     public String getPassword() {
-        return password;
+        return password; // retrieving password
     }
     public String getRole(){
-       return role;
+       return role; // retrieving role
     }
     public int getUserID(){
-       return userID;
+       return userID; // retrieving userID
     }
-    
+
+
+    // Setter - User will be able to reset their own user/pass
+    public void setPassword(String newPassword) {
+        if (newPassword.equals(this.password)) {
+            System.out.println("Please choose a different password.");
+        } else {
+            this.password = newPassword;
+        }
+    }
+
+
+    // Setter method to reset username
+
+
     // Method to create account
     public static User newUser(String username, String password, String role, Integer userID) {
        return new User(username, password, role, userID);
     }
 
 
-
-
-
-
-
     // Method to reset username
-
-
-
-
 
 
 
