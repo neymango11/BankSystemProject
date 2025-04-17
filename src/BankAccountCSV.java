1d82c3ca7510635fc515bf0b89e0da33d9d1c78f
Aman Mohamed <amanmoe221@gmail.com>
2025-04-17 12:22:58 -0400

    update

--- src/BankAccountCSV.java
+++ src/BankAccountCSV.java
@@ -7,13 +7,13 @@ public class BankAccountCSV {
      * EXAMPLE - Aman-C-1001,1001,500.0,SAVING
      *
      *
-     *  create file path to csv file
+     *  CREATE file path to csv file
      *
-     *  create a method that will write to the csv file
+     *  CREATE a method that will write to the csv file
      *
-     *  create a method that will delete from csv file (ADMIN)
+     *  CREATE a method that will delete from csv file (ADMIN)
      *
-     *  create a method that will read all BankAccounts form csv file (ADMIN)
+     *  CREATE a method that will read all BankAccounts form csv file (ADMIN)
      *
      *
      * */
---
+++ src/DemoMain.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class DemoMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Creating a user object to use
+        //User aman = new User( "Aman", "M", "STANDARD", null);
+        //User nebil = new User("Nebil", "M", "STANDARD", null );
+
+
+        User Aman = User.newUser()
+
+    }
+}
